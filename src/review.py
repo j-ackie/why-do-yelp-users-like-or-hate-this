72d1e081bbc8f0e0e07d8a61509fa84727dd28cb
Jackie Lin <jackielin2025@u.northwestern.edu>
2022-02-27 09:44:15 -0600

    Renamed file, fixed up review.py

--- src/review.py
+++ src/review.py
@@ -3,73 +3,82 @@ import os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
-
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 
 class Review:
-    def __init__(self, review):
-        divs = review.find_all("div")
+    """
+    Represents a Yelp review.
+    """
+    def __init__(self, li):
+        divs = li.find_all("div")
         for div in divs:
             if "aria-label" in div.attrs and "star rating" in div.attrs["aria-label"]:
                 self.rating = int(div.attrs["aria-label"][0])
-        self.text = review.find("p", {"class": "comment__09f24__gu0rG css-1sufhje"}).text
+        print(li.find("p", {"class": "comment__09f24__gu0rG css-1sufhje"}).text)
+        self.text = li.find("p").text
 
 
 def search_businesses(search_term: str):
     """
-    Returns search results for input
-    Helper function for get_yelp_reviews
+    Returns search results for input.
+    Helper function for get_yelp_reviews.
 
     https://www.yelp.com/developers/documentation/v3/business_search
+
     :param search_term: What user would like to search for
-    :return:
+    :return: Dictionary containing all the businesses pertaining to search term
     """
     url = "https://api.yelp.com/v3/businesses/search"
     headers = {"Authorization": "Bearer {}".format(API_KEY)}
     params = {"term": search_term,
               "location": "New York City"}
     try:
-        result = requests.get(url, headers=headers, params=params, timeout=10)
+        result = requests.get(url, headers=headers, params=params)
         result.raise_for_status()
     except requests.RequestException:
+        print("Search business error")
         return None
-
+    
     return result.json()["businesses"]
 
-# Add bool for if they want to query for specific restaurant or not
+
 def get_yelp_reviews(url: str):
     """
-    Returns the yelp reviews for a search term
-    Helper function
-    :param url:
-    :return:
+    Returns the yelp reviews on a single page for a search term.
+    Helper function for get_all_yelp_reviews.
+
+    :param url: Yelp url of search term
+    :return: List of Review objects for a single page
     """
 
     print("Loading " + url)
     try:
-        result = requests.get(url, timeout=10)
+        result = requests.get(url)
         result.raise_for_status()
     except requests.RequestException:
+        print("ERROR")
         return None
 
     soup = BeautifulSoup(result.content, "html.parser")
 
-    reviews = soup.find_all("li", {"class": "margin-b5__09f24__pTvws border-color--default__09f24__NPAKY"})
+    reviews = []
 
-    cleaned_reviews = []
-    for review in reviews:
-        cleaned_reviews.append(Review(review))
+    list_items = soup.find_all("li", {"class": "margin-b5__09f24__pTvws border-color--default__09f24__NPAKY"})
+    for li in list_items:
+        if li.find("p", {"class": "comment__09f24__gu0rG css-1sufhje"}):
+            reviews.append(Review(li))
 
-    return cleaned_reviews
+    return reviews
 
 
 def get_all_yelp_reviews(search_term: str):
     """
+    Returns the yelp reviews on every page for a search term
 
-    :param search_term:
-    :return:
+    :param search_term: What user would like to search for
+    :return: List of Review objects for every page
     """
     all_reviews = []
     counter = 0
@@ -77,9 +86,6 @@ def get_all_yelp_reviews(search_term: str):
         reviews = get_yelp_reviews(search_businesses(search_term)[0]["url"] + f"&start={counter}")
         if len(reviews) == 0:
             return all_reviews
+        print(len(reviews))
         all_reviews.append(reviews)
         counter += 10
-
-
-if __name__ == "__main__":
-    print(get_all_yelp_reviews("McDonalds")[0][0].text)
