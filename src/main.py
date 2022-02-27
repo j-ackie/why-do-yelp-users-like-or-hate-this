8367fcabffeeab3582564faeb601c131f78623a6
Jackie Lin <jackielin2025@u.northwestern.edu>
2022-02-27 10:15:42 -0600

    Implemented tokenize function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
-from review import get_all_yelp_reviews
+import re
 
+
+def tokenize(text):
+    """
+
+    :param text:
+    :return:
+    """
+    tokens = [re.sub(r'[^A-Za-z]+', '', s).lower().strip() for s in  text.split()]
+    return tokens
+
+#def calc_like(search_term: str):
 
