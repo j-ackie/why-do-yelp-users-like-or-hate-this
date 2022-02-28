214f6c9eb7d6df26c3ea6cf13bef26779a8cb52e
Jackie Lin <jackielin2025@u.northwestern.edu>
2022-02-28 16:03:51 -0600

    Imported unit test, added some test cases for search_businesses

--- test/test_review.py
+++ test/test_review.py
@@ -1,4 +1,15 @@
-import src.review
+from src.review import search_businesses
+import unittest
 
-def tokenize_test():
-    
+
+class TestReviewFunctions(unittest.TestCase):
+    def test_search_businesses(self):
+        # Expect empty list because zero results
+        self.assertEqual(search_businesses("qwertyuiop"), [])
+
+        # Check that top result's name is Cheeky Sandwiches
+        self.assertEqual(search_businesses("Sandwich")[0]["name"], "Cheeky Sandwiches")
+
+
+if __name__ == "__main__":
+    unittest.main()
