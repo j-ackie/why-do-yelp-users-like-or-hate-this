6848ef327c38d463b20dc8a35b1bd2d33d3abaf5
Jackie Lin <jackielin2025@u.northwestern.edu>
2022-03-01 09:59:00 -0600

    Minor fix

--- src/app.py
+++ src/app.py
@@ -15,19 +15,22 @@ def index():
 
 @app.route("/<search_term>", methods=["GET", "POST"])
 def results(search_term):
-    result = load_freqs(search_term)
-    business_name = result[0]
-    business_url = result[1]
-    freqs = result[2]
-    if len(freqs["positive"]) < 10:
-        pos_freqs = freqs["positive"][0:len(freqs["positive"])]
-    else:
-        pos_freqs = freqs["positive"][0:10]
+    if request.method == "GET":
+        result = load_freqs(search_term)
+        business_name = result[0]
+        business_url = result[1]
+        freqs = result[2]
+        if len(freqs["positive"]) < 10:
+            pos_freqs = freqs["positive"][0:len(freqs["positive"])]
+        else:
+            pos_freqs = freqs["positive"][0:10]
 
-    if len(freqs["negative"]) < 10:
-        neg_freqs = freqs["negative"][0:len(freqs["negative"])]
+        if len(freqs["negative"]) < 10:
+            neg_freqs = freqs["negative"][0:len(freqs["negative"])]
+        else:
+            neg_freqs = freqs["negative"][0:10]
+        return render_template("results.html", business_name=business_name, business_url=business_url,
+                               pos_freqs=pos_freqs, neg_freqs=neg_freqs)
     else:
-        neg_freqs = freqs["negative"][0:10]
-    return render_template("results.html", business_name=business_name, business_url=business_url,
-                           pos_freqs=pos_freqs, neg_freqs=neg_freqs)
+        return redirect("/")
 
